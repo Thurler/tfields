300a863ec8a40df58d4c603e498a1a33cbe7fcd4
Thurler <guithurler90@gmail.com>
2025-03-03 01:04:42 -0300

    Added convenient first and last getters for string

--- lib/extensions/string.dart
+++ lib/extensions/string.dart
@@ -7,4 +7,12 @@ extension StringExtension on String {
   String upperCaseFirstChar() {
     return replaceRange(0, 1, this[0].toUpperCase());
   }
+
+  /// Returns the first character in the string, or empty if the string is
+  /// empty
+  String get first => isNotEmpty ? substring(0, 1) : '';
+
+  /// Returns the last character in the string, or empty if the string is
+  /// empty
+  String get last => isNotEmpty ? substring(length - 1) : '';
 }
