f454c52b4cf5aa6c2fc6fc3b438b81c334a534d9
Thurler <guithurler90@gmail.com>
2025-11-06 14:18:51 -0300

    Added missing comments to TimeTracker

--- lib/mixins/time_tracker.dart
+++ lib/mixins/time_tracker.dart
@@ -6,11 +6,16 @@ import 'package:flutter/material.dart';
 /// can be started, stopped and resumed to keep track of how many seconds have
 /// passed since it was started
 mixin TTimeTracker<T extends StatefulWidget> on State<T> {
+  /// The timer that will tick every second when tracking is active
   Timer? _timer;
+
+  /// The number of seconds elapsed since the timer was started
   int elapsedSeconds = 0;
 
+  /// Whether the State is currently tracking time or not
   bool get timerIsActive => _timer?.isActive ?? false;
 
+  /// The duration of time elapsed since the timer was started
   Duration get elapsedDuration => Duration(seconds: elapsedSeconds);
 
   @override
